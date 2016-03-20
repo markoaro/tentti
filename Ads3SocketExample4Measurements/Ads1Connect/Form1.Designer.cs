5339ec3a5c775287ce50b27d0d7bbffca59d73e4
markoaro <marko.aro@seamk.fi>
2016-03-20 21:31:43 +0200

    Added files via upload

---
+++ Ads3SocketExample4Measurements/Ads1Connect/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace Ads1Connect
+{
+    partial class AdsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonSetSR1 = new System.Windows.Forms.Button();
+            this.buttonResetSR1 = new System.Windows.Forms.Button();
+            this.textBoxStructData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(118, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonSetSR1
+            // 
+            this.buttonSetSR1.Location = new System.Drawing.Point(14, 47);
+            this.buttonSetSR1.Name = "buttonSetSR1";
+            this.buttonSetSR1.Size = new System.Drawing.Size(117, 23);
+            this.buttonSetSR1.TabIndex = 7;
+            this.buttonSetSR1.Text = "Start";
+            this.buttonSetSR1.UseVisualStyleBackColor = true;
+            this.buttonSetSR1.Click += new System.EventHandler(this.buttonSetSR1_Click);
+            // 
+            // buttonResetSR1
+            // 
+            this.buttonResetSR1.Location = new System.Drawing.Point(137, 47);
+            this.buttonResetSR1.Name = "buttonResetSR1";
+            this.buttonResetSR1.Size = new System.Drawing.Size(117, 23);
+            this.buttonResetSR1.TabIndex = 8;
+            this.buttonResetSR1.Text = "Stop";
+            this.buttonResetSR1.UseVisualStyleBackColor = true;
+            this.buttonResetSR1.Click += new System.EventHandler(this.buttonResetSR1_Click);
+            // 
+            // textBoxStructData
+            // 
+            this.textBoxStructData.Location = new System.Drawing.Point(107, 113);
+            this.textBoxStructData.Name = "textBoxStructData";
+            this.textBoxStructData.Size = new System.Drawing.Size(654, 20);
+            this.textBoxStructData.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 116);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Struct data";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(22, 160);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(739, 300);
+            this.chart1.TabIndex = 11;
+            this.chart1.Text = "chart1";
+            // 
+            // AdsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 472);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxStructData);
+            this.Controls.Add(this.buttonResetSR1);
+            this.Controls.Add(this.buttonSetSR1);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "AdsForm";
+            this.Text = "Ads test 2";
+            this.Load += new System.EventHandler(this.AdsForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonSetSR1;
+        private System.Windows.Forms.Button buttonResetSR1;
+        private System.Windows.Forms.TextBox textBoxStructData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Ads3SocketExample4Measurements/Ads1Connect/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO; // Otetaan ads kirjasto mukaan
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TwinCAT.Ads;
+
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Ads1Connect
+{
+    public partial class AdsForm : Form
+    {
+        // jäsenmuuttujat
+        // viittaus adsClient-olioon
+        private TcAdsClient adsClient;
+        private AdsStream adsStream;
+        private BinaryReader binReader;
+
+        // kahvat TwinCAT-muuttujiin
+        private int startHandle;
+        private int stopHandle;
+        private int measurementDataHandle;
+        private int stepHandle;
+
+        private int cntr = 0;
+
+        public AdsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AdsForm_Load(object sender, EventArgs e)
+        {
+            buttonConnect.Enabled = true;
+            buttonSetSR1.Enabled = false;
+            buttonResetSR1.Enabled = false;
+
+            DataPoint dp1 = new DataPoint(1, 1);
+            DataPoint dp2 = new DataPoint(2, 2);
+            DataPoint dp3 = new DataPoint(3, 1);
+
+            DataPoint dp4 = new DataPoint(1, 1.2);
+            DataPoint dp5 = new DataPoint(2, 2.4);
+            DataPoint dp6 = new DataPoint(3, 1.1);
+
+            chart1.Series.Clear();
+            chart1.Series.Add("Measurement 1");
+            chart1.Series.Add("Measurement 2");
+
+            /*
+            chart1.Series[0].Points.Add(dp1);
+            chart1.Series[0].Points.Add(dp2);
+            chart1.Series[0].Points.Add(dp3);
+
+            chart1.Series[1].Points.Add(dp4);
+            chart1.Series[1].Points.Add(dp5);
+            chart1.Series[1].Points.Add(dp6);
+             */
+
+            chart1.Series[0].ChartType =
+                    SeriesChartType.Line;
+            chart1.Series[1].ChartType =
+                    SeriesChartType.Line;
+             
+
+            chart1.Series[0].Color = Color.Red;
+            chart1.Series[1].Color = Color.Blue;
+
+            for (int i = 0; i < 100; i++)
+            {
+                xbuff[i] = i;
+                y1buff.Enqueue(0);
+                y2buff.Enqueue(0);
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            // luodaan AdsClient-olio yhteyttä varten
+            adsClient = new TcAdsClient();
+            // otetaan yhteys TwinCAT PLC:hen portin 851 kautta
+            adsClient.Connect(851); 
+
+            // luodaan PLC:n muuttujiin viittaavat "osoittimet"
+            startHandle = adsClient.CreateVariableHandle("MAIN.start");
+            stopHandle = adsClient.CreateVariableHandle("MAIN.reset");
+
+            // Luodaan tapahtumankäsittelijä. Parametrina näyttöä päivittävän metodin nimi
+            adsClient.AdsNotification += new AdsNotificationEventHandler(UpdateVariables);
+
+            // avataan Ads-stream (luettava byte-määrä pitänee antaa parametrina
+            this.adsStream = new AdsStream(24);
+            binReader = new BinaryReader(this.adsStream);
+
+            /*
+                timeHi: UDINT; 		// aika
+                timeLo: UDINT;	 		// aika
+                measurement1: REAL;	// mittaus 1
+                measurement2: REAL;	// mittaus 2
+                measurement3: REAL;	// mittaus 3
+                counter: INT;			// juokseva numero
+                arrayValue: INT;		// jotain
+             * */
+            measurementDataHandle = adsClient.AddDeviceNotification("MAIN.measurementData", this.adsStream, 0, 24,
+                                        AdsTransMode.Cyclic, 100, 0, null);
+
+
+            buttonConnect.Enabled = false;
+            buttonSetSR1.Enabled = true;
+            buttonResetSR1.Enabled = false;
+        }
+
+        private Queue<double> y1buff = new Queue<double>();
+        private Queue<double> y2buff = new Queue<double>();
+        private int[] xbuff = new int[100];
+
+        private void UpdateVariables(object sender, AdsNotificationEventArgs e)
+        {
+            // e.NotificationHandlen avulla tunnistetaan tapahtuman aiheuttava TwinCAT-muuttuja
+            if (e.NotificationHandle == measurementDataHandle)
+            {
+                string aika = e.TimeStamp.ToString();// e.Value.ToString();
+                long x = long.Parse(aika);
+                DateTime dt = new DateTime(x);
+
+                e.DataStream.Position = e.Offset;
+
+                // lue structin tiedot BinaryReaderin avulla
+                UInt32 timeHi = binReader.ReadUInt32();
+                UInt32 timeLo = binReader.ReadUInt32();
+                double m1 = binReader.ReadSingle();
+                double m2 = binReader.ReadSingle();
+                double m3 = binReader.ReadSingle();
+
+                int counter = binReader.ReadInt16();
+                int joku = binReader.ReadInt16();
+
+                /*
+                chart1.Series[0].Points.AddXY(cntr++, m1);
+                chart1.Series[1].Points.AddXY(cntr++, m2);
+                 */
+                y1buff.Enqueue(m1);
+                y2buff.Enqueue(m2);
+                if (y1buff.Count > 100 )
+                {
+                    y1buff.Dequeue();
+                }
+                if (y2buff.Count > 100)
+                {
+                    y2buff.Dequeue();
+                }
+                
+                
+                chart1.Series[0].Points.DataBindXY(xbuff, "x1", y1buff, "y1");
+                chart1.Series[1].Points.DataBindXY(xbuff, "x2", y2buff, "y2");
+
+                textBoxStructData.Text = counter + " " + m1 + " " + m2 + " " + m3;
+            }
+ 
+        }
+
+        private void buttonSetSR1_Click(object sender, EventArgs e)
+        {
+            // Laitetaan Start päälle
+            adsClient.WriteAny(startHandle, true);
+            // Odotetaan 0.5 s
+            System.Threading.Thread.Sleep(500);
+            // laitetaan Stop päälle
+            adsClient.WriteAny(startHandle, false);
+
+            buttonConnect.Enabled = false;
+            buttonSetSR1.Enabled = false;
+            buttonResetSR1.Enabled = true;
+        }
+
+        private void buttonResetSR1_Click(object sender, EventArgs e)
+        {
+            // Laitetaan Start päälle
+            adsClient.WriteAny(stopHandle, true);
+            // Odotetaan 0.5 s
+            System.Threading.Thread.Sleep(500);
+            // laitetaan Stop päälle
+            adsClient.WriteAny(stopHandle, false);
+
+            buttonConnect.Enabled = false;
+            buttonSetSR1.Enabled = true;
+            buttonResetSR1.Enabled = false;
+        }
+
+
+    }
+}
